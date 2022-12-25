47517ac7771582b471fe51e7ddd19088a4d86638
LizBTU <121333837+LiziChichuaGeoLab@users.noreply.github.com>
2022-12-25 11:13:15 +0400

    Setup Project

--- Lecture18/Scenes/Movies/MoviesViewController.swift
+++ Lecture18/Scenes/Movies/MoviesViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MoviesViewController.swift
 //  Lecture18
 //
 //  Created by LiziChichua on 25.12.22.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MoviesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
